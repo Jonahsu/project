fcdc15e80506b56714aad2ae545dfcd489a6869b
Jonahsu <125948720+Jonahsu@users.noreply.github.com>
2023-03-10 11:18:09 +0530

    Add files via upload

---
+++ WebImage.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebImage.aspx.cs" Inherits="Q2image.WebImage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+    <body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="ddlItems" runat="server" AutoPostBack="true" 
+                OnSelectedIndexChanged="ddlItems_SelectedIndexChanged">
+                <asp:ListItem Text="Select an item" Value="0" />
+                <asp:ListItem Text="Item 1" Value="1" />
+                <asp:ListItem Text="Item 2" Value="2" />
+                <asp:ListItem Text="Item 3" Value="3" />
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Image ID="imgItem" runat="server" Height="150" Width="150" />
+            <br />
+            <br />
+            <asp:Button ID="btnCost" runat="server" Text="Get Cost" OnClick="btnCost_Click" />
+            <br />
+            <br />
+            <asp:Label ID="lbCost" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebImage.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Q2image
+{
+    public partial class WebImage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Set the default selection of the dropdown list.
+                ddlItems.SelectedIndex = 0;
+            }
+
+        }
+        protected void ddlItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Get the selected item from the dropdown list.
+            int selectedItem = int.Parse(ddlItems.SelectedValue);
+
+            // Set the image URL based on the selected item.
+            switch (selectedItem)
+            {
+                case 1:
+                    imgItem.ImageUrl = "images/image.jfif";
+                    break;
+                case 2:
+                    imgItem.ImageUrl = "images/image2.jfif";
+                    break;
+                case 3:
+                    imgItem.ImageUrl = "images/image3.jfif";
+                    break;
+                default:
+                    imgItem.ImageUrl = "";
+                    break;
+            }
+        }
+
+        protected void btnCost_Click(object sender, EventArgs e)
+        {
+            // Get the selected item from the dropdown list.
+            int selectedItem = int.Parse(ddlItems.SelectedValue);
+
+            // Set the cost of the selected item.
+            switch (selectedItem)
+            {
+                case 1:
+                    lbCost.Text = "Item 1 costs 110.";
+                    break;
+                case 2:
+                   lbCost.Text = "Item 2 costs 520.";
+                    break;
+                case 3:
+                    lbCost.Text = "Item 3 costs 930.";
+                    break;
+                default :
+                    lbCost.Text = "Item 3 costs 930.";
+                    break;
+
+            }
+        }
+    }
+}
